701c5d1d97053c64828a63186931d5be114f4faf
TheLondonWhale <thomas.perus@espeme.com>
2018-10-08 19:25:06 +0200

    fin exo 6

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -10,20 +10,23 @@ array_words.each do |word|
     if vowels.include?(word[0]) #apple#
       word = word + 'ay'
       modified_words << word
-    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2]) #apple#
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2]) #three et school
       word = word[3..-1] + word[0..2] + 'ay'
       modified_words << word
-    elsif consonants.include?(word[0]) && consonants.include?(word[1]) ##
+    elsif consonants.include?(word[0]) && "qu".include?(word[1..2]) #square
+      word = word[3..-1] + word[0..2] + 'ay'
+      modified_words << word
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) # cherry / the / brown
       word = word[2..-1] + word[0..1] + 'ay'
       modified_words << word
-    elsif consonants.include?(word[0]) && vowels.include?(word[1]) && vowels.include?(word[2]) && vowels.include?(word[3]) #apple#
+    elsif consonants.include?(word[0]) && vowels.include?(word[1]) && vowels.include?(word[2]) && vowels.include?(word[3]) #quiet
       word = word[2..-1] + word[0..1] + 'ay'
       modified_words << word
-    elsif consonants.include?(word[0]) && vowels.include?(word[1]) && vowels.include?(word[2]) && vowels.include?(word[3]) #apple#
+    elsif "qu".include?(word[0..1]) #many words (force le quick)
       word = word[2..-1] + word[0..1] + 'ay'
       modified_words << word
     elsif consonants.include?(word[0])
-      word = word[1..-1] + word[0] + 'ay' #banana#
+      word = word[1..-1] + word[0] + 'ay' #banana / fox
       modified_words << word
     else
     end
